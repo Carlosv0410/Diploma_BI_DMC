148269b82dd9f3babb246409be4efad5436d6259
Carlos Carrillo Villavicencio <69774730+Carlosv0410@users.noreply.github.com>
2026-06-01 20:22:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ st.title("Proyecto Final Diploma BI")
 
 st.sidebar.title("Parámetros")
 
-st.image("Python_logo.png")
-st.sidebar.image("DMC.png")
+st.image("Python_logo.png",width = 50)
+st.sidebar.image("DMC.png",width = 30)
