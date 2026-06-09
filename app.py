2a12a5c5f63e44c005af777ca3db6559e104821d
Carlos Carrillo Villavicencio <69774730+Carlosv0410@users.noreply.github.com>
2026-06-08 19:48:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.sidebar.image("DMC.png", width=100)
 # Mostramos un texto indicando el autor del proyecto
 st.write("Elaborado por: Carlos Carrillo")
 
-modulos = st.selectbox("Selecione un módulo", ["Home","Carga y perfil del dataset","Procesamiento de datos","Análisis visual"])
+modulos = st.sidebar.selectbox("Selecione un módulo", ["Home","Carga y perfil del dataset","Procesamiento de datos","Análisis visual"])
 
 
 # Creamos un cargador de archivos para subir archivos Excel o CSV
